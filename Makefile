d0ac6111981f4de7fc2df496c313671716a903d1
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-12-13 13:50:54 +0200

    implemented optel tracing and metrics

--- Makefile
+++ Makefile
@@ -32,6 +32,28 @@ migrate-up:
 migrate-down:
 	migrate -path backend/infrastructure/storage/pgstore/migrations -database "postgresql://postgres:bersen@localhost/postgres?sslmode=disable" -verbose down
 
+docker-compose-up:
+	docker compose -f backend/infrastructure/docker/docker-compose.yml up -d
+
+docker-compose-down:
+	docker compose -f backend/infrastructure/docker/docker-compose.yml down
+
+prom-run:
+	docker run --name prom \
+		-v $PWD/backend/infrastructure/prom/prometheus.yml:/etc/prometheus/prometheus.yml \
+		-p 9090:9090 --add-host=host.docker.internal:host-gateway prom/prometheus:v2.29.2
+
+jaeger-run: 
+	docker run --name jaeger \
+	-p 5775:5775/udp \
+	-p 6831:6831/udp \
+	-p 6832:6832/udp \
+	-p 5778:5778 \
+	-p 16686:16686 \
+	-p 14268:14268 \
+	-p 14250:14250 \
+	-p 9411:9411 \
+	jaegertracing/all-in-one:1.29.0
 
 psql:
 	docker exec -it testpgdb psql $(PSQLURL)
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/bersennaidoo/etracker/backend/infrastructure/logger"
 	"github.com/bersennaidoo/etracker/backend/infrastructure/storage/pgstore"
 	"github.com/bersennaidoo/etracker/backend/physical/config"
 	"github.com/bersennaidoo/etracker/backend/physical/conn"
+	m "github.com/bersennaidoo/etracker/backend/physical/opentelem/metric"
+	t "github.com/bersennaidoo/etracker/backend/physical/opentelem/trace"
 )
+
+const serviceName = "etracker"
 
 func main() {
 	l := flag.Bool("local", false, "true - send to stdout, false - send to logging server")
 	flag.Parse()
+
+	sTracing, err := t.InitTracing(serviceName)
+	if err != nil {
+		log.Fatalf("Failed to setup tracing: %v\n", err)
+	}
+	defer func() {
+		if err := sTracing(context.Background()); err != nil {
+			log.Printf("Failed to shutdown tracing: %v\n", err)
+		}
+	}()
+
+	sMetrics, err := m.InitMetrics(serviceName)
+	if err != nil {
+		log.Fatalf("Failed to setup metrics: %v\n", err)
+	}
+	defer func() {
+		if err := sMetrics(context.Background()); err != nil {
+			log.Printf("Failed to shutdown metrics: %v\n", err)
+		}
+	}()
 
 	logger.SetLoggingOutput(*l)
 
